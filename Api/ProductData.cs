ee9d7188e4aea4f4978647c916c4e24edc8b1dfa
Hamberfim <adhamlingithub@gmail.com>
2021-06-28 11:46:12 -0500

    updated product itemw

--- Api/ProductData.cs
+++ Api/ProductData.cs
@@ -38,6 +38,13 @@ namespace Api
                 Name = "Apples",
                 Description = "Bag of 7 fresh McIntosh apples",
                 Quantity = 1
+            },
+            new Product
+            {
+                Id = 40,
+                Name = "Oranges",
+                Description = "1 lbs. box/bag of  fresh oranges",
+                Quantity = 1
             }
         };
 
